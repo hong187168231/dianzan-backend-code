dcae9e7cd85a8c8cac06bce2c885acc28f6d9816
puff <yinpuff@gmail.com>
2023-01-17 17:25:12 +0800

    1

--- likes-common/src/main/java/com/likes/common/model/request/AgentOrderReq.java
+++ likes-common/src/main/java/com/likes/common/model/request/AgentOrderReq.java
@@ -1,9 +1,15 @@
 package com.likes.common.model.request;
 
 
+import lombok.Data;
+
+@Data
 public class AgentOrderReq {
 
     private String email;
+
+    private String acclogin;
+
     private String accno;
     /**
      * 订单编号
@@ -21,83 +27,4 @@ public class AgentOrderReq {
     private String beginTime;
     private String endTime;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAccno() {
-        return accno;
-    }
-
-    public void setAccno(String accno) {
-        this.accno = accno;
-    }
-
-    public String getOrderno() {
-        return orderno;
-    }
-
-    public void setOrderno(String orderno) {
-        this.orderno = orderno;
-    }
-
-    public String getOrderstatus() {
-        return orderstatus;
-    }
-
-    public void setOrderstatus(String orderstatus) {
-        this.orderstatus = orderstatus;
-    }
-
-    public String getUniqueId() {
-        return uniqueId;
-    }
-
-    public void setUniqueId(String uniqueId) {
-        this.uniqueId = uniqueId;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getBeginTime() {
-        return beginTime;
-    }
-
-    public void setBeginTime(String beginTime) {
-        this.beginTime = beginTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getSuperiorId() {
-        return superiorId;
-    }
-
-    public void setSuperiorId(String superiorId) {
-        this.superiorId = superiorId;
-    }
 }
